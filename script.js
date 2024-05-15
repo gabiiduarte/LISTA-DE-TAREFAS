2aa6f31f5cfe3fd815496fea61624f0f98922f39
gabiiduarte <125580472+gabiiduarte@users.noreply.github.com>
2024-05-15 09:34:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function addTask() {
+    var input = document.getElementById("taskInput").value;
+    if (input === "") {
+        alert("Por favor, insira uma tarefa!");
+        return;
+    }
+    var ul = document.getElementById("taskList");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input));
+    li.onclick = function() {
+        li.classList.toggle("completed");
+    };
+    ul.appendChild(li);
+    document.getElementById("taskInput").value = "";
+}
+
 
